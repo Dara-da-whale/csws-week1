0c6fbffcb2280d194afa70be4fd5eca37c1e0589
Dara-da-whale <CSMDOMIW@ljmu.ac.uk>
2023-01-27 12:05:21 +0000

    final test

--- week1.py
+++ week1.py
@@ -1,2 +1,16 @@
-for x in range(10):
-  print("Hello world" + str(x))
+
+names = ['John', 'Sam','Ethan','Ethans mum','harry']
+for i in range (5):
+ print("Hello " + names[i])
+
+ location = ['paris','spain','germany','africa','russia']
+ print(sorted(location))
+
+ location.reverse()
+ print(location)
+ 
+ food = ['Burger','Chips','pizza','Spag','jellof','CHicken']
+ print(food)
+ 
+ food.insert['apple']
+ print(food)
